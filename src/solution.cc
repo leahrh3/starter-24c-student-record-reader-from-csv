0207ca7805887c50a95537b743cc866ca25b4534
leahrh3 <leahrh3@illinois.edu>
2025-09-16 13:32:14 -0500

    Update solution.cc

--- src/solution.cc
+++ src/solution.cc
@@ -2,21 +2,21 @@
 #include <fstream>
 
 Student ReadStudentRecFromStream(std::istream& is) {
-  if (!is.isopen()) {
+  if (!is.is_open()) {
     std::cerr << "Could not open" << is << std::endl;
     return Student{};
   }
   while (is.good()) {
-    string first_name = "";
-    string last_name = "";
+    std::string first_name = "";
+    std::string last_name = "";
     unsigned int uin = 0;
     float gpa = 0;
     char throw_away = '\0';
     if (is.fail()) {
       return Student{};
     }
-    string full_name = first_name + " " + last_name;
-    return Student{full_name, uin, gpa);
+    std::string full_name = first_name + " " + last_name;
+    return Student{full_name, uin, gpa};
   }
   return Student{};
 }
